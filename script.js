9e4ddc6df631f8b170c11bda16ac40d2258f448e
Manu Devappa <manudevappas@gmail.com>
2025-07-06 00:55:58 +0530

    Updated contact section and resume button

---
+++ script.js
@@ -0,0 +1,39 @@
+fetch("header.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+
+        // Attach search logic after header is loaded
+        const searchBox = document.getElementById("searchInput");
+        if (searchBox) {
+          searchBox.addEventListener("keypress", function (e) {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              const query = searchBox.value.trim();
+              if (query) {
+                alert("You searched for: " + query);
+              }
+            }
+          });
+        }
+      });
+
+      fetch("header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
+
+      // Attach search logic after header is loaded
+      const searchBox = document.getElementById("searchInput");
+      if (searchBox) {
+        searchBox.addEventListener("keypress", function (e) {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            const query = searchBox.value.trim();
+            if (query) {
+              alert("You searched for: " + query);
+            }
+          }
+        });
+      }
+    });
