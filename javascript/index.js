58bff65aaf66e9207f63621c78644f8951c4d248
Manu Devappa <manudevappas@gmail.com>
2025-08-10 22:33:38 +0530

    added 2 backround  images for hero section

--- javascript/index.js
+++ javascript/index.js
@@ -17,3 +17,33 @@
       }
     );
     observer.observe(contactSection);
+
+
+    
+
+
+    const images = [
+  "images/image1.jpg",
+  "images/image.png",
+  "images/a.png"
+];
+
+let currentIndex = 0;
+const bgElement = document.querySelector('.hero-coder .bg');
+
+function changeBackground() {
+  bgElement.style.opacity = 0; // fade out
+
+  setTimeout(() => {
+    bgElement.style.backgroundImage = `url('${images[currentIndex]}')`;
+    currentIndex = (currentIndex + 1) % images.length;
+    bgElement.style.opacity = 1; // fade in
+  }, 2000); // fade-out duration
+}
+
+// First image
+bgElement.style.backgroundImage = `url('${images[currentIndex]}')`;
+currentIndex++;
+
+// Change every 8 seconds → 2s fade out + 4s stay + 2s fade in
+setInterval(changeBackground, 8000);
